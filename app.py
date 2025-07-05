e77a9fe1cb91ff32c620330dd09af778fe406f44
karthik-kannan1617 <karthikpradeepkarthikpradeep4@gmail.com>
2025-07-05 09:32:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/gallery')
+def gallery():
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='your_mysql_user',
+        password='your_password',
+        database='portfolio'
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT filename, caption FROM photos")
+    photos = cursor.fetchall()
+    conn.close()
+    return render_template('gallery.html', photos=photos)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
